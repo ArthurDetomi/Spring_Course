ad930b7459e9093fd76e1ae268b5140392a3294f
Arthur Detomi <arthurdetomi@gmail.com>
2023-10-04 20:53:27 -0300

    Seção 11: Lidando com migrations

--- spring_course_udemy/src/main/java/br/com/arthur/controllers/PersonController.java
+++ spring_course_udemy/src/main/java/br/com/arthur/controllers/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/person/v1")
 public class PersonController {
 
     @Autowired
